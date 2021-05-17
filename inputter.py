ac153a6873c33ab7f468ce8f27efb4d77ef946fd
DakshYadav358 <61241857+DakshYadav358@users.noreply.github.com>
2021-05-17 16:49:38 +0530

    Add files via upload

--- inputter.py
+++ inputter.py
@@ -16,15 +16,46 @@ Inpput()
 #print(arr[10]['Username'])
 names=[]
 final={}
+dates=[]
+
+def extractDates():
+    for i in arr:
+        if i['Date'] not in dates:
+            dates.append(i['Date'])
+    #print(dates)
+extractDates()
 
 
 
 
-
-dat="05/10/21"
 for i in arr:
-    if dat== i['Date']:
-        #print(i['Username'])
-        names.append(i['Username'])
-    final['dat']=names
-print(final['dat'])
+    print('Hi')
+    print(i['Username'])
+    dat='05/10/21'
+    if i[f'{dat}'] in final:
+        print('Hi')
+    else:
+        print('hello')
+
+l='''
+def dictFinal():
+    dat=dates
+    
+    for i in arr:
+        if i['Date'] in dates:
+            #print(i['Username'])
+            names.append(i['Username'])
+    final['{dat}'.format(dat=dat)]=names
+    #print(final)
+'''
+
+
+
+print(dates)
+
+#dictFinal()
+
+print(final)
+
+
+#dictFinal('05/10/21')
