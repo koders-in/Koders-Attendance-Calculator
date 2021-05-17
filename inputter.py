67a7ddb6424b1d11562fd55ed4de9885b274c9f5
DakshYadav358 <61241857+DakshYadav358@users.noreply.github.com>
2021-05-17 14:59:15 +0530

    Add files via upload
    Input successfully parsed

--- inputter.py
+++ inputter.py
@@ -4,11 +4,29 @@ import pandas as pd
 
 arr=[]
 
+
 def Inpput():
     boston=pd.read_csv('C:/Users/ASUS/Attendance/Koders-Attendance-Calculator/Attendance_Sheet.csv')
+    l=len(boston)
+    i=0
     for ind, row in boston.iterrows():
-        if ind==0:
-            print(row)
+        while i<l:
+            #print(row)
             arr.append(row)
+            i+=1
 Inpput()
 
+def reall():
+    l=len(arr)
+    c=0
+    dat=""
+    print(dat)
+    
+    for i in arr:
+        if dat=="":
+            dat=i[0]
+            print(dat)
+            
+        
+
+reall()
