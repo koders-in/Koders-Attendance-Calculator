0132aedd6d65249c6cd1be04f536651e6d7aa973
DakshYadav358 <61241857+DakshYadav358@users.noreply.github.com>
2021-05-18 17:28:53 +0530

    added a function for the total attendance count without repetetion

--- inputter.py
+++ inputter.py
@@ -1,30 +1,92 @@
+import csv 
+import json  
+import pandas as pd
+
 import sys,os
+file_address=os.path.join(sys.path[0], "Attendance_Sheet.csv")
+
+#print(file_address)
+
+arr=[]
+
+#'C:/Users/ASUS/Attendance/Koders-Attendance-Calculator/Attendance_Sheet.csv'
+def Inpput():
+    boston=pd.read_csv(file_address)
+    l=len(boston)
+    i=0
+    for ind, row in boston.iterrows():
+        arr.append(row)
+            
+Inpput()
+
+final={}
+dates=[]
+Timer=[]
+
+def extract_Dates():
+    for i in arr:
+        if i['Date'] not in dates:
+            dates.append(i['Date'])
 
 
-initial_data=[]
-final_data=[]
+def extract_Time():
+    for i in arr:
+        if i['Time'] not in Timer:
+            Timer.append(i['Time'])
 
-def Input_CSV():
-    with open(file_address, "r") as file:
-        for i in file.readlines():
-            initial_data.append(i)
+extract_Time() 
 
-def split_data():
-    for i in initial_data:
-        final_data.append(i.split(","))
-    del final_data[0]
+
+
+#print(Timer)
+extract_Dates()
+#print(dates)
+
+def To_Dict():
+    c=0
+    r=0
+    for j in dates:
+        names=[]
+        for i in arr:
+            t=i['Time']
+            t1=t[0:2]
+            
+
+            #print(t1,t2,t3)
+            if i['Date'] == j and int(t1)==11 and str(t1) in i['Time']:
+                names.append(i['Username'])
+                names=list(set(names))
+            elif i['Date'] == j and int(t1)==15 and str(t1) in i['Time']:
+                names.append(i['Username'])
+                names=list(set(names))
+            c+=1
+            final[f'{j}']=names
+            if(r<len(dates)-1):
+                r=r+1
+            
+
+To_Dict()
+#print(final)   
+
+attenden={}
+
+def attend():
+    
+    for i in dates:
+        j=0
+        l=len(final.get(i))
         
+        while j<l:
+            n=final.get(i)[j]
+            j+=1
+            
+            if n not in attenden:
+                attenden[f'{n}']=1
+            else:
+                attenden[f'{n}']+=1
+    print(attenden)
 
-Input_CSV()
 
-split_data()
-def Search_by_name(name):
-    name=name
-    counter=0
-    for i in final_data:
-        #print(i[2])
-        if i[2].startswith(name):
-            counter=counter+1
-    print(counter)
-
-Search_by_name("BenZee")
+    
+    
+attend()
