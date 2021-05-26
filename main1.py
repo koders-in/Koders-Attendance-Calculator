ced8af42ad35b41cb7819e94586d210dfbcc0871
deepakm78 <84370956+deepakm78@users.noreply.github.com>
2021-05-26 15:11:57 +0530

    Update main1.py
    updated weekly bar_graph

--- main1.py
+++ main1.py
@@ -1,17 +1,19 @@
 import sys,os
 import datetime
-from matplotlib import lines
+from typing import Counter
+
+
+
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
-import matplotlib.style as style
-style.available
 
 #date = datetime.datetime(int(year), int(month), 1)
 
 # GLOBAL VALUES
 data=[]
+dates=[]
+names=[]
 attendance={}
+attendees={}
 
 def input_csv():
     with open("data.csv", "r") as file:
@@ -19,61 +21,69 @@ def input_csv():
             data.append(line.split(","))
         del data[0]
 
-# TODO
-# Handle unique values
+def to_get_dates():
+    for each in data:
+        if each[0] not in dates:
+            dates.append(each[0])
 
-def search_by_name(name):
-    try:
-        name=name
-        counter, total_counter = 0,0
-        for each in data:
-            total_counter += 1 # bug here
-            if each[2].startswith(name):
-                counter=counter+1
-        print("Searched name: " + name ,", " + str(counter) + "/" + str(total_counter))
-    except Exception as error:
-        print("Something went wrong")
-        print("Error" + str(error))
+def to_get_names():
+
+    for each in data:
+        if each[2] not in names:
+            names.append(each[2])
+
+
+
 
 
 def to_get_dict():
-    names=[]
-    #creating a dictionary for unique items as atendance
-    for each in data:
-        splitter=each[1].split(':')
-        if  splitter[0]=='11':
-            names.append(each[2])
-            names=list(set(names))
-        elif splitter[0]=='15':
-            names.append(each[2])
-            names=list(set(names))        
-        attendance[f'{each[0]}']=names
+    
+    
+    for eachh in dates:
+        legnth_of_data_set=len(data)
+        names1=[]
+        names2=[]
+        temp=0
+        while temp<legnth_of_data_set:
+            if eachh==data[temp][0] and data[temp][1].startswith('11'):
+                names1.append(data[temp][2])
+                names1=list(set(names1))
+            elif eachh==data[temp][0] and data[temp][1].startswith('15'):
+                names2.append(data[temp][2])
+                names2=list(set(names2))
+            temp+=1
+        attendance[f'{eachh}']=[names1,names2]      
+    
+    return(attendance)
 
-def overall_attendance():
-    temp=0
-    attendees={}
+def search_by_name(name):
     try:
-    #creatung a dictionary for unique items as atendance
-        to_get_dict()
-        keysList = list(attendance.keys())
-        #counting each person's attendance and storing in atendees
-        for each in keysList:
-            temp=0
-            legnth=len(attendance.get(each))     
-            while temp<legnth:
-                names=attendance.get(each)[temp]            
-                if names not in attendees:
-                    attendees[f'{names}']=1
-                else:
-                    attendees[f'{names}']+=1
-                temp+=1
-        print(attendees)
+        counter, total_counter = 0, len(dates*2)
+        for each in dates:
+            for eachh in attendance[f'{each}']:
+                for eachhh in eachh:
+                    if name in eachhh:
+                        counter+=1
+                
+            
+        
+        return counter
     except Exception as error:
         print("Something went wrong")
-    
+        print("Error" + str(error))
+def overall_attendance():
+     
+    try:
+        for each in names:
+            count=0
+            count=search_by_name(each)
+            attendees[f'{each}']=count
+        return attendees
+    except Exception as error:
+        print("Something went wrong")
+
 def weekly_attendance():
     current_date = datetime.datetime.today()
-    attendees = {}
     week_dates = []
     dated = []
     temp = 0
@@ -90,16 +100,16 @@ def weekly_attendance():
         if day != "Sunday" and day != "Saturday":
             formatted_time = each.strftime("%m/%d/%y")
             dated.append(str(formatted_time))
-    print(dated)
+    
     for each in dated:
         try:
             print(attendance[f'{each}'])
         except Exception as error:
-            print("Error" + str(error))
-    
+            print("No data Found" + str(error))
+    return dated
+
 def yearly_attendance():
     current_date = datetime.datetime.today()
-    attendees = {}
     week_dates = []
     dated = []
     temp = 0
@@ -116,22 +126,21 @@ def yearly_attendance():
         if day != "Sunday" and day != "Saturday":
             formatted_time = each.strftime("%m/%d/%y")
             dated.append(str(formatted_time))
-    print(dated)
+    
     for each in dated:
         try:
             print(attendance[f'{each}'])
         except Exception as error:
-            print("Error" + str(error))
-    
+            print("No data found" + str(error))
+    return(dated)
 
 def monthly_attendance():
     current_date = datetime.datetime.today()
-    attendees = {}
     week_dates = []
     dated = []
     temp = 0
 
-    # to get object of dates of last 30 days
+    # to get object of dates of last 7 days
     while temp < 30:
         previous_date = current_date - datetime.timedelta(days=temp)
         week_dates.append(previous_date)
@@ -143,64 +152,146 @@ def monthly_attendance():
         if day != "Sunday" and day != "Saturday":
             formatted_time = each.strftime("%m/%d/%y")
             dated.append(str(formatted_time))
-    print(dated)
+    
     for each in dated:
         try:
             print(attendance[f'{each}'])
         except Exception as error:
-            print("Error" + str(error))
+            print("No data Found" + str(error))
+    return(dated)
+
+
+def visualize_bar_overall_attendance():
+    x=[]
+    y=[]
+    for each in names:
+        count=search_by_name(each)
+        y.append(count)
+        x.append(each)
+    y.append((len(dates)*2))
+    x.append('Total shifts')
+    plt.barh(x, y)
+    plt.title('TOTAL ATTENDANCE')
+    plt.show()
+
+def visualize_pie_graph_search_by_name(name):
+    count=search_by_name(name)
+    print(count)
+    y = [len(dates*2),count]
+    mylabels = [f"{name} {count}",f"overall attendance {len(dates*2)}"]
+
+    plt.pie(y, labels = mylabels,  startangle = 0)
+    plt.title('TOTAL ATTENDANCE OF A SINGLE PERSON')
+    plt.show() 
+
+def weeklY_bar():
+    weekly_attendees={}
+    dated=weekly_attendance()
+    #print(dated)
+    for each in dated:
+        
+        for name in names:
+            counter=0
+            for eachh in attendance[f'{each}']:
+                
+                for eachhh in eachh:
+                    
+                    if name in eachhh:
+                        counter+=1
+                    weekly_attendees[f'{name}']=counter
+
+    y =list(weekly_attendees.values())
+
+    x =list(weekly_attendees.keys())
+    
+    plt.barh(x,y)
+    plt.title('TOTAL WEELY ATTENDANCE')
+    plt.show()
+
+def monthly_bar():
+    monthly_attendees={}
+    
+    dated=monthly_attendance()
+    for each in dated:
+        
+        for name in names:
+            counter=6
+            for eachh in attendance[f'{each}']:
+                
+                for eachhh in eachh:
+                    
+                    if name in eachhh:
+                        counter+=1
+                monthly_attendees[f'{name}']=counter
+
+    y =list(monthly_attendees.values())
+
+    x =list(monthly_attendees.keys())
     
 
-# TODO
-# def visualize_bar
-
-# TODO
-# def visualize_pie
-
-# TODO
-# Name: Benzee
-# Date: 12/05/20
-# Shift: 1- Morning, 2- Post Lunch
-# Present
-# Absent
-
-# TODO
-# cli conversion
+    plt.barh(x,y)
+    plt.title('TOTAL ATTENDANCE')
+    plt.show()
 
 
-# search_by_name("BenZee") # For testing
+def cli_conversion():
+    while True:
+        print('Do you want to continue: Y/N')
+        print("For Attendance by name press 1")
+        print("For total attendance press 2")
+        print("For weekly attendance press 3")
+        print("For monthly attendance press 4")
+        print('For yearly attendance press 5')
+        print('For overall attenadce graph press 6')
+        print('For pie graph for person press 7')
+        print('For weekly bar graph of week press 8')
+
+        choice=int(input('Enter:  '))
+
+        if choice ==1:
+            name=input('Enter name:   ')
+            counter=search_by_name(name)
+            print("Searched name: " + name ,", " + str(counter) + "/" + str(len(dates)*2))
+        elif choice ==2:
+            print(overall_attendance())
+        elif choice ==3:
+            weekly_attendance()
+        elif choice ==4:
+            monthly_attendance()
+        elif choice ==5:
+            yearly_attendance()
+        elif choice ==6:
+            visualize_bar_overall_attendance()
+        elif choice ==7:
+            visualize_pie_graph_search_by_name(input('Enter name:   '))
+        elif choice ==8:
+            weeklY_bar()
+        else:
+            print('wrong input')
+
+        print('Do you want to continue: Y/N')
+        to_continue=input('Enter:   ').capitalize()
+        if to_continue=='N':
+            break
+
+
 
 # Driver code
 if __name__ == '__main__':
     input_csv()
+    to_get_dates()
+    to_get_names()
     to_get_dict()
-    # search_by_name('BenZee')
+    # search_by_name('Ritesh')
     # overall_attendance()
-    weekly_attendance()
-    # monthly_attendance()
-    # yearly_attendance()
+    # weekly_attendance()
+    #visualize_bar_weekly_attendance()
+    #monthly_attendance()
+    #yearly_attendance()
+    cli_conversion()
+    visualize_bar_overall_attendance()
+    # visualize_pie_graph_search_by_name('XHunter')
+    weeklY_bar()
+    monthly_bar()
 
-#After reading csv file we moving through graph ploting....!
-style.use('fivethirtyeight')
-fig=plt.figure()
-axi=fig.add_subplot(1,1,1)
-def graph(i):
-    graph_data=open('data.csv','r').read()
-    lines=graph_data.split('.')
-    xs=[]
-    ys=[]
-    for line in lines:
-        if len(line)> 1:
-            x, y=line.splite('.')
-            xs.append(x)
-            ys.append(y)
-    ax1.clear()
-    ax1.plt(ax,ys)
 
-ani=animation.FuncAnimation(fig,graph,interval=10)
-plt.show()
-ani=animation.FuncAnimation(fig,graph,interval=1000)
-plt.show()
-# TODO
-#deepak check the to_det_dict function to get the correct output
-#check the output by matching with data.csv to see it is showing correct output according to entries on a particular day
